eeb604e3cc9e0b2e36b205e3f9223c94e40d3931
Rmvzzuser <119730501+Rmvzzuser@users.noreply.github.com>
2023-01-05 17:02:35 +0530

    Update banned.py

--- Natalie/banned.py
+++ Natalie/banned.py
@@ -21,17 +21,17 @@ disabled_group=filters.create(disabled_chat)
 @Client.on_message(filters.private & banned_user & filters.incoming)
 async def ban_reply(bot, message):
     ban = await db.get_ban_status(message.from_user.id)
-    await message.reply(f'Sorry Dude, You are Banned to use Me. \nBan Reason: {ban["ban_reason"]}')
+    await message.reply(f'Sorry Poor Boi , U R Banned to use Me.\n Ban Reason: {ban["ban_reason"]}')
 
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
     buttons = [[
-        InlineKeyboardButton('Support', url=f'https://telegram.dog/{SUPPORT_CHAT}')
+        InlineKeyboardButton('Support', url=f'https://telegram.dog/Off_topic_discussion')
     ]]
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
     k = await message.reply(
-        text=f"CHAT NOT ALLOWED 🐞\n\nMy admins has restricted me from working here ! If you want to know more about it contact support..\nReason : <code>{vazha['reason']}</code>.",
+        text=f"ALERT:-   CHAT NOT ALLOWED\n\n\nReason: Do aks Admins For MOre Help \n\nContact Support <code>{vazha['reason']}</code>.",
         reply_markup=reply_markup)
     try:
         await k.pin()
